22b6fb9b03d90713bb1d8a3bf0f069d1170e55e7
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-06 19:57:54 -0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist/js'), // absolute path, buildin note package
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'), // absolute path, buildin note package
+    filename: 'js/bundle.js'
   },
-  mode: 'development'
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 };
