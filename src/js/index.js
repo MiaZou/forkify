9198c137fe2916b3e6acbf928d5358a8cbeaf957
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-07 13:50:16 -0800

    api setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,35 @@
 // Global app controller
-import num from './test';
-const x = 23;
+import Search from './models/Search';
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}.`);
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from the view
+  const query = 'pizza'; //TODO
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
