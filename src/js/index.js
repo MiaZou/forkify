b7c1bf3369824187b1543063277225d0cbcc7cc2
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-09 21:32:14 -0700

    controller like

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
@@ -125,11 +126,46 @@ elements.shopping.addEventListener('click', e => {
 
     // Delete from UI
     listView.deleteItem(id);
+
+    // Handle update
   } else if (e.target.closest('.shopping__count-value')) {
     const val = parseFloat(e.target.value, 10);
     state.list.updateCount(id, val);
   }
 });
+
+
+// Like Controller
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+
+  const currentID = state.recipe.id;
+  // user has NOT yet liked current recipe
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    );
+
+    // Toggle the like button
+
+    // Add like to UI list
+    console.log(state.likes);
+
+  // user has Liked current recipe
+  } else {
+    // Remote like to the state
+    state.likes.deleteLike(currentID);
+
+    // Toggle the like button
+
+    // Remove like to UI list
+    console.log(state.likes);
+  }
+}
 
 
 // Handling recipe button clicks
@@ -145,7 +181,11 @@ elements.recipe.addEventListener('click', e => {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
   } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    // Add ingredients to shopping List
     controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Like controller
+    controlLike();
   }
 
 });
