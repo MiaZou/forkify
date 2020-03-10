3dfac91ad21c769c40bbd85922c1a59056b50518
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-09 17:32:25 -0700

    render recipe

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Global app controller
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -13,6 +15,8 @@ import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
+
+// Search Controller
 const controlSearch = async () => {
   // 1) Get query from the view
   const query = searchView.getInput(); //TODO
@@ -26,12 +30,17 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
 
-    // 5) Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+      // 5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert('Something wrong with the search...');
+      clearLoader();
+    }
   }
 }
 
@@ -48,3 +57,46 @@ elements.searchResPages.addEventListener('click', e => {
     searchView.renderResults(state.search.result, goToPage);
   }
 });
+
+
+// Recipe Controller 
+
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
+      state.recipe.parseIngredients();
+
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+
+      // Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+
+    } catch(error) {
+      alert('Error processing recipe!');
+    }
+  }
+};
+
+
+['hashchange', 'loader'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+
+
